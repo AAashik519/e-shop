9545adcef3202ad9b3ae567eabe39f327ef4c175
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-08-12 00:14:46 +0600

    image url error

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,24 @@
- import React from 'react'
- import {client} from '../lib/client'
- import { Product,FooterBanner,HeroBanner } from '../components'
- const 
- Home = ({products ,bannerData}) => {
-   return (
-    <>
-    <HeroBanner heroBanner ={bannerData.length && bannerData[0] }  />
-    
-    <div className='products-heading'>
-      <h2>Best selling Products</h2>
-      <p>Speakers of many variations </p>
-     </div>
-     
+import React from 'react';
 
-     <div className='products-container'>
-     {products?.map((product)=>product.name)}
-     </div>
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
 
- <FooterBanner />
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
 
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
-    
-    </>
-   )
- }
 
  export const getServerSideProps = async()=>{
   const query = '*[_type == "product"]' ;
