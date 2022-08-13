e9bc59f2e76ed9e5134826816939d2ac8bb4542e
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-08-14 04:20:48 +0600

    url error solved

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = ({ products, bannerData }) => (
     </div>
 
     <div className="products-container">
-      {products?.map((product) => <Product key={product._id} product={product} />)}
+      {products?.map((product) => <Product key={product._id} product={product} /> )}
     </div>
 
     <FooterBanner footerBanner={bannerData && bannerData[0]} />
@@ -21,15 +21,16 @@ const Home = ({ products, bannerData }) => (
 
 
  export const getServerSideProps = async()=>{
-  const query = '*[_type == "product"]' ;
+   const query = '*[_type == "product"]' ;
   const products = await client.fetch(query);
 
   const  bannerQuery = '*[_type == "banner"]' ;
-  const  bannerData  = await client.fetch(bannerQuery)
+  const  bannerData  = await client.fetch(bannerQuery);
 
   return {
     props :{
-      products,bannerData
+      products,
+      bannerData
     }
   }
  }
